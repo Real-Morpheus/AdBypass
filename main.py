68abeb05a0dea7fcadeb3664c45f06af825fba4c
Real-Morpheus <skvishwakarma583@gmail.com>
2024-10-12 18:01:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import PyBypass as bypasser
 
 st.set_page_config(page_title="URL Bypasser", page_icon='🧊',
                    layout="centered", initial_sidebar_state="auto",    menu_items={
-                       'Get Help': 'https://telegram.me/oksurya',
-                       'Report a bug': "https://telegram.me/oksurya",
-                       'About': "This is URL Bypasser for ADLINKFLY based website. (https://github.com/oksurya)"
+                       'Get Help': 'https://telegram.me/RealMorpheuss',
+                       'Report a bug': "https://telegram.me/RealMorpheuss",
+                       'About': "This is URL Bypasser for ADLINKFLY based website. (https://github.com/real-morpheus"
                    })
 
 
@@ -16,40 +16,8 @@ def random_celeb():
     return random.choice([st.balloons()])
 
 st.title("URL Bypasser")
-st.text('Deployed by Jayasurya Mailsamy')
+st.text('Devloped By Sanchit Vishwakarma')
 
 tab1, tab2 = st.tabs(["Bypass", "Available Websites", ])
 
 __avl_website__ = ['try2link.com', ' adf.ly', ' bit.ly', ' ouo.io', ' ouo.press', ' shareus.in', ' shortly.xyz', ' tinyurl.com', ' thinfi.com', ' hypershort.com ', 'safeurl.sirigan.my.id', ' gtlinks.me', ' loan.kinemaster.cc', ' theforyou.in', ' linkvertise.com', ' shorte.st', ' earn4link.in', ' tekcrypt.in', ' link.short2url.in', ' go.rocklinks.net', ' rocklinks.net', ' earn.moneykamalo.com', ' m.easysky.in', ' indianshortner.in', ' open.crazyblog.in', ' link.tnvalue.in',' shortingly.me', ' open2get.in', ' dulink.in', ' bindaaslinks.com', ' za.uy', ' pdiskshortener.com', ' mdiskshortner.link', ' go.earnl.xyz', ' g.rewayatcafe.com', ' ser2.crazyblog.in', ' bitshorten.com', ' rocklink.in', ' droplink.co', ' tnlink.in', ' ez4short.com', ' xpshort.com', ' vearnl.in', ' adrinolinks.in', ' techymozo.com', ' linkbnao.com', ' linksxyz.in', ' short-jambo.com', ' ads.droplink.co.in', ' linkpays.in', ' pi-l.ink', ' link.tnlink.in ', ' pkin.me']
-
-with tab1:
-    show_alert = False
-    url = st.text_input(label="Paste your URL")
-    if st.button("Submit"):
-        if url:
-            try:
-
-                with st.spinner("Loading..."):
-                    bypassed_link = bypasser.bypass(url)
-                    st.success(bypassed_link)
-
-                random_celeb()
-
-                with st.expander("Copy"):
-                    st.code(bypassed_link)
-
-            except (UnableToBypassError, BypasserNotFoundError, UrlConnectionError) as e:
-                if show_alert := True:
-                    st.error(e)
-
-            if st.button("Dismiss"):
-                show_alert = False
-
-        elif show_alert := True:
-            st.error("No URLS found")
-
-with tab2:
-    st.subheader("Available Websites")
-    st.table(__avl_website__)
-
-
